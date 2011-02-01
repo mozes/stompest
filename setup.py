ad2e4f48e71585fbbd748054768672dcb9273722
Roger Hoover <roger.hoover@gmail.com>
2011-02-01 09:20:42 -0800

    Bumped to 1.0.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read(fname):
 
 setup(
     name = "stompest",
-    version = "1.0.0",
+    version = "1.0.1",
     author = "Roger Hoover",
     author_email = "roger.hoover@gmail.com",
     description = "STOMP client library for Python including both synchronous and Twisted implementations.",
