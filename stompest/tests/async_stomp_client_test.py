af1d5eedb53e40f41bb646c88b264aa33894677d
Roger Hoover <roger.hoover@gmail.com>
2012-07-24 17:36:39 -0700

    Fixed copyright notices to retain original year

--- stompest/tests/async_stomp_client_test.py
+++ stompest/tests/async_stomp_client_test.py
@@ -1,6 +1,6 @@
 # -*- coding: iso-8859-1 -*-
 """
-Copyright 2012 Mozes, Inc.
+Copyright 2011, 2012 Mozes, Inc.
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
