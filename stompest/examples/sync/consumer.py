6531972dc3477da6026849af91fbb239d47f4b0c
Jan Müller <jpmueller@gmx.net>
2012-10-27 03:06:47 +0200

    updated example for the usage of sync client

--- stompest/examples/sync/consumer.py
+++ stompest/examples/sync/consumer.py
@@ -1,5 +1,6 @@
+# -*- coding: iso-8859-1 -*-
 """
-Copyright 2011 Mozes, Inc.
+Copyright 2011, 2012 Mozes, Inc.
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
@@ -13,12 +14,19 @@ Copyright 2011 Mozes, Inc.
    See the License for the specific language governing permissions and
    limitations under the License.
 """
-from stompest.simple import Stomp
+from stompest.protocol import StompConfig
+from stompest.sync import Stomp
 
+CONFIG = StompConfig('tcp://localhost:61613')
 QUEUE = '/queue/simpleTest'
 
-stomp = Stomp('localhost', 61613)
+stomp = Stomp(CONFIG)
 stomp.connect()
-stomp.send(QUEUE, 'test message1')
-stomp.send(QUEUE, 'test message2')
+stomp.subscribe(QUEUE, {'ack': 'client'})
+
+while True:
+    frame = stomp.receiveFrame()
+    print "Got message frame: %s" % repr(frame)
+    stomp.ack(frame)
+    
 stomp.disconnect()
