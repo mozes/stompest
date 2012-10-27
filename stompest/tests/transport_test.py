c7ac177f07dc495172a6288af8d2dcf2e1e14768
Jan Müller <jpmueller@gmx.net>
2012-10-27 17:32:24 +0200

    added tet cases for StompFrameTransport (not connected send/receive)

--- stompest/tests/transport_test.py
+++ stompest/tests/transport_test.py
@@ -37,16 +37,39 @@ class StompFrameTransportTest(unittest.TestCase):
         while True:
             yield ''
 
-    def _recv(self, iterable, size):
-        return ''.join(itertools.islice(iterable, size))
-    
     def _get_receive_mock(self, stream):
         transport = StompFrameTransport(HOST, PORT)
+        connected = transport._connected = Mock()
+        connected.return_value = True
         socket = transport._socket = Mock()
         stream = self._generate_bytes(stream)
-        socket.recv = Mock(wraps=lambda size, *_: self._recv(stream, size))
+        socket.recv = Mock(wraps=lambda size: ''.join(itertools.islice(stream, size)))
         return transport
     
+    def _get_send_mock(self):
+        transport = StompFrameTransport(HOST, PORT)
+        connected = transport._connected = Mock()
+        connected.return_value = True
+        transport._socket = Mock()
+        return transport
+    
+    def test_send(self):
+        frame = StompFrame('MESSAGE')
+                
+        transport = self._get_send_mock()
+        transport.send(frame)
+        self.assertEquals(1, transport._socket.sendall.call_count)
+        args, _ = transport._socket.sendall.call_args
+        self.assertEquals(str(frame), args[0])
+        
+    def test_send_not_connected_raises(self):
+        frame = StompFrame('MESSAGE')
+                
+        transport = self._get_send_mock()
+        transport._connected.return_value = False
+        self.assertRaises(StompConnectionError, transport.send, frame)
+        self.assertEquals(0, transport._socket.sendall.call_count)
+        
     def test_receive(self):
         headers = {'x': 'y'}
         body = 'testing 1 2 3'
@@ -60,6 +83,12 @@ class StompFrameTransportTest(unittest.TestCase):
         self.assertRaises(StompConnectionError, transport.receive)
         self.assertEquals(transport._socket, None)
 
+    def test_receive_not_connected_raises_and_removes_socket(self):
+        transport = self._get_receive_mock('Hi')
+        transport._connected.return_value = False
+        self.assertRaises(StompConnectionError, transport.receive)
+        self.assertEquals(None, transport._socket)
+        
     def test_receive_multiple_frames_extra_newlines(self):
         headers = {'x': 'y'}
         body = 'testing 1 2 3'
